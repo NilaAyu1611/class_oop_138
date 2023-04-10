0473d3e4ea2a37c9504f370c920667aecf144b8c
NilaAyu1611 <nilaayucw@gmail.com>
2023-04-10 10:48:43 +0700

    buat akses modifier dari variabel nim, nama

--- classoop138/classoop138.cpp
+++ classoop138/classoop138.cpp
@@ -1,11 +1,7 @@
-// classoop138.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
-
-/
+class Mahasiswa {
+public:
+	string nama;
+	string nim;
